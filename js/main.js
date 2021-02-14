797ea313907016772569a733af67e18a79c61ae7
Natali-front <78704034+Natali-front@users.noreply.github.com>
2021-02-14 23:23:42 +0300

    website files

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let backButton = document.querySelector('.slick-next');
+let nextButton = document.querySelector('.slick-prev');
+
+$('.adv-type').slick({
+    dots: true,
+    arrows: true,
+    
+})
+
+    
+   
